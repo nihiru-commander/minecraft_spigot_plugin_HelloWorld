d0b9349eabe81d900dbf6bd7f699e07d630b4c64
nihiru_yuuki <83627705+nihiru-commander@users.noreply.github.com>
2023-09-10 08:59:25 +0900

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,17 @@
+package watyaserver.net.untitled;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlayerStatistics extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getLogger().info("Hello World!");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
